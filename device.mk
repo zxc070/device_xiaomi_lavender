4a2938a43038414460190dd0ba4dcfe310635e71
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    lavender: overlay: Enable option for full screen aspect ratio
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
