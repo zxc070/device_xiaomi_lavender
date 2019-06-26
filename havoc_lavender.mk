0a300fe203ac68bfcae328c4c5ca411115c57fcb
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-06-26 11:41:55 +0700

    lavender: Update build fingerprint to V10.3.6.0 stable
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- havoc_lavender.mk
+++ havoc_lavender.mk
@@ -30,9 +30,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.5.0.PFGMIXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.5.0.PFGMIXM release-keys" \
+    TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    TARGET_DEVICE="lavender"
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.6.0.PFGMIXM release-keys"
+
+BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.6.0.PFGMIXM:user/release-keys"
