1b73c21e85b2989a0b70acee5a00ff4153df0818
TheStrechh <arriagagarciajc@gmail.com>
2019-08-15 16:53:43 -0500

    lavender: Remove NFC configs

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,7 +18,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m \
-    ro.hardware.nfc_nci=nqx.default \
     persist.radio.multisim.config=dsds \
     persist.vendor.qcomsysd.enabled=1 \
     keyguard.no_require_sim=true \
